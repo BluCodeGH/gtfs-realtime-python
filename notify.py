569873442fe7dc86640965c21fc91b0b7cccfd57
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2019-09-20 17:56:57 -0400

    Double check times, add minimum arrival time

--- notify.py
+++ notify.py
@@ -5,17 +5,26 @@ from datetime import datetime, timedelta
 import gtfs
 
 dt = timedelta(minutes=int(sys.argv[1]))
-if len(sys.argv) > 2:
-  cmd = " ".join(sys.argv[2:])
+start = datetime.utcnow() + timedelta(minutes=int(sys.argv[2]))
+if len(sys.argv) > 3:
+  cmd = " ".join(sys.argv[3:])
 else:
   cmd = None
 
+double = False
 while True:
   gtfs.fetch()
   for route, time in gtfs.get(1820, [13, 6]):
-    if datetime.utcnow() + dt > time:
+    if datetime.utcnow() + dt > time > start:
       print(gtfs.show(time) + "\t" + route)
+      if not double:
+        double = True
+        break
       if cmd:
         os.system(cmd.replace("{}", route + ": " + gtfs.show(time)))
       sys.exit(0)
-  sleep(60)
+  else:
+    double = False
+    sleep(60)
+    continue
+  sleep(30)
